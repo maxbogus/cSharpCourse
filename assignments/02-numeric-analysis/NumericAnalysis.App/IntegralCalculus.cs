b227c33c0a1aad6987e690a795954d43f63f9d49
Maxim Boguslavskiy <max.bogus@gmail.com>
2024-04-14 23:22:59 +0300

    implemented second lab

--- assignments/02-numeric-analysis/NumericAnalysis.App/IntegralCalculus.cs
+++ assignments/02-numeric-analysis/NumericAnalysis.App/IntegralCalculus.cs
@@ -4,11 +4,29 @@ namespace NumericAnalysis;
 
 public class IntegralCalculus
 {
+    private static double IntegralCalculate(Func<double, double> f, double a, double b, int n) {
+        double hStep = (b - a) / n;
+        double sum = 0.5*(f(a) + f(b));
+        for (int i = 1; i < n; ++i) {
+            double x = a+i*hStep;
+            sum += f(x);
+        }
+        return sum * hStep;
+    }
+
     public static double Calculate(Func<double, double> func, double x1, double x2, double precision)
     {
-        double y1 = func(x1);
-        double y2 = func(x2);
+        bool shouldExit = false;
+        double steps = 1.0;
 
-        throw new NotImplementedException();
+        while (!shouldExit) {
+            if (((x2 - x1) / steps) <= precision) {
+                shouldExit = true;
+            } else {
+                steps *= 2;
+            }
+        }
+
+        return IntegralCalculate(func, x1, x2, (int)steps);
     }
 }
